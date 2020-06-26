9478b941b2c3dd243d4ed2f83eae88112cb15c97
nicoledbianchin <nicoledbianchin@gmail.com>
2020-06-26 10:40:08 -0300

    adiciona dependencia nodemon

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const app = require('./src/config/custom-express');
 
 app.listen(3000, function() {
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,5 +1,3 @@
-const app = require("../../config/custom-express");
-
 module.exports = (app) => {
 
     app.get('/', function(req, resp) {
